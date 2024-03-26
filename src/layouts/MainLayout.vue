411443cbf924af0933b137cdd26f8d1ce8f37050
Mahder Desta <mahderds2013@gmail.com>
2024-03-26 16:36:00 +0300

    ui: prepare layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,42 +1,186 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+  <q-layout view="hHh Lpr lFf" class="bg-white">
+    <q-header class="bg-white text-grey-8 q-py-xs" height-hint="58">
       <q-toolbar>
         <q-btn
           flat
           dense
           round
+          class="text-dark"
           icon="menu"
           aria-label="Menu"
           @click="toggleLeftDrawer"
+          v-if="!$q.screen.gt.xs"
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-btn
+          flat
+          dense
+          round
+          class="text-dark"
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleMinState"
+          v-if="$q.screen.gt.xs"
+        />
 
-        <div>Quasar v{{ $q.version }}</div>
+        <q-btn flat dense no-caps class="q-ml-xs" v-if="$q.screen.gt.xs">
+          <q-icon :name="fabYoutube" color="red" size="28px" />
+          <q-toolbar-title shrink class="text-weight-bold">
+            YouTube
+          </q-toolbar-title>
+        </q-btn>
+
+        <q-space />
+
+        <div class="YL__toolbar-input-container row no-wrap">
+          <q-input
+            dense
+            outlined
+            rounded
+            v-model="search"
+            placeholder="Search"
+            class="bg-white col"
+          />
+          <q-btn
+            class="YL__toolbar-input-btn"
+            color="grey-3"
+            text-color="grey-8"
+            icon="search"
+            unelevated
+            rounded
+          />
+          <q-btn
+            round
+            dense
+            flat
+            color="grey-8 "
+            style="right: 70px"
+            icon="keyboard"
+          />
+        </div>
+        <q-btn round dense flat color="grey-8 q-ml-xs" icon="mic" />
+
+        <q-space />
+
+        <div class="q-gutter-sm row items-center no-wrap">
+          <q-btn round dense flat color="grey-8" icon="more_vert">
+            <q-tooltip>More Actions</q-tooltip>
+          </q-btn>
+
+          <q-btn
+            style="border: 1px solid; border-radius: 20px"
+            flat
+            class="q-px-md text-blue-8"
+            v-if="$q.screen.gt.sm"
+          >
+            <q-avatar icon="account_circle" size="26px"> </q-avatar>
+            User
+            <q-tooltip>Account</q-tooltip>
+          </q-btn>
+          <q-btn
+            round
+            dense
+            flat
+            color="grey-8"
+            icon="invert_colors"
+            v-if="$q.screen.gt.sm"
+          >
+            <q-tooltip>Theme</q-tooltip>
+          </q-btn>
+        </div>
       </q-toolbar>
     </q-header>
 
     <q-drawer
       v-model="leftDrawerOpen"
       show-if-above
-      bordered
+      :mini="miniState"
+      :mini-width="80"
+      class=""
+      :width="200"
+      :breakpoint="667"
     >
-      <q-list>
-        <q-item-label
-          header
+      <q-scroll-area class="fit">
+        <q-btn
+          flat
+          dense
+          no-caps
+          class="q-ml-sm q-mt-sm"
+          v-if="!$q.screen.gt.xs"
         >
-          Essential Links
-        </q-item-label>
+          <q-icon :name="fabYoutube" color="red" size="28px" />
+          <q-toolbar-title shrink class="text-weight-bold">
+            YouTube
+          </q-toolbar-title>
+        </q-btn>
+        <q-list padding>
+          <q-item clickable v-ripple>
+            <q-item-section class="column justify-center" avatar>
+              <q-icon name="home" class="self-center" />
+              <span
+                class="self-center text-caption text-weight-light"
+                v-if="miniState && $q.screen.gt.xs"
+                >Home</span
+              >
+            </q-item-section>
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+            <q-item-section> Home </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section class="column justify-center" avatar>
+              <q-icon name="music_note" class="self-center" />
+              <span
+                class="text-center self-center text-caption text-weight-light"
+                v-if="miniState && $q.screen.gt.xs"
+                >Shorts</span
+              >
+            </q-item-section>
+
+            <q-item-section> Shorts </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section class="column justify-center" avatar>
+              <q-icon name="ondemand_video" class="self-center" />
+              <span
+                class="text-center self-center text-caption text-weight-light"
+                v-if="miniState && $q.screen.gt.xs"
+                >Tv</span
+              >
+            </q-item-section>
+
+            <q-item-section> TV </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section class="column justify-center" avatar>
+              <q-icon name="library_music" class="self-center" />
+              <span
+                class="text-center self-center text-caption text-weight-light"
+                v-if="miniState && $q.screen.gt.xs"
+                >Channels</span
+              >
+            </q-item-section>
+
+            <q-item-section> Channels </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section class="column justify-center" avatar>
+              <q-icon name="history" class="self-center" />
+              <span
+                class="text-center self-center text-caption text-weight-light"
+                v-if="miniState && $q.screen.gt.xs"
+                >History</span
+              >
+            </q-item-section>
+
+            <q-item-section> History </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
     </q-drawer>
 
     <q-page-container>
@@ -46,61 +190,54 @@
 </template>
 
 <script setup lang="ts">
+import { fabYoutube } from '@quasar/extras/fontawesome-v6';
+
 import { ref } from 'vue';
-import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
 
 defineOptions({
-  name: 'MainLayout'
+  name: 'MainLayout',
 });
-
-const linksList: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
 
 const leftDrawerOpen = ref(false);
 
-function toggleLeftDrawer () {
+const miniState = ref(true);
+const search = ref('');
+
+function toggleMinState() {
+  miniState.value = !miniState.value;
+}
+
+function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 }
 </script>
+
+<style lang="sass">
+.YL
+
+  &__toolbar-input-container
+    min-width: 100px
+    width: 55%
+    position: relative
+
+  &__toolbar-input-btn
+    position: absolute
+    right: 0
+    top: 0
+    bottom: 0
+    border-style: solid
+    border-width: 1px 1px 1px 0
+    border-color: rgba(0,0,0,.24)
+    max-width: 60px
+    width: 100%
+
+
+  &__drawer-footer-link
+    color: inherit
+    text-decoration: none
+    font-weight: 500
+    font-size: .75rem
+
+    &:hover
+      color: #000
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,47 +1,90 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="q-pa-md q-px-lg">
+    <div class="row items-start q-col-gutter-lg">
+      <div
+        v-for="video in videos"
+        :key="video.id"
+        class="col-12 col-sm-6 col-md-4 col-lg-3"
+      >
+        <video-component :video="video"></video-component>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import { Video } from 'components/models';
+import VideoComponent from 'src/components/VideoComponent.vue';
 
 defineOptions({
-  name: 'IndexPage'
+  name: 'IndexPage',
 });
 
-const todos = ref<Todo[]>([
+const videos = ref<Video[]>([
   {
     id: 1,
-    content: 'ct1'
+    title: 'Episode 1: I challenged everything',
+    channel: 'Korean Podcast',
+    channel_image: 'https://cdn.quasar.dev/img/avatar2.jpg',
+    thumbnail_image: 'https://cdn.quasar.dev/img/parallax1.jpg',
+    views: 12,
   },
   {
     id: 2,
-    content: 'ct2'
+    title: 'Episode 2: Discover the world',
+    channel: 'Travel Adventures',
+    channel_image: 'https://cdn.quasar.dev/img/avatar2.jpg',
+    thumbnail_image: 'https://cdn.quasar.dev/img/parallax2.jpg',
+    views: 34,
   },
   {
     id: 3,
-    content: 'ct3'
+    title: 'Cooking Masterclass: How to make pizza',
+    channel: "Chef's Kitchen",
+    channel_image: 'https://cdn.quasar.dev/img/avatar2.jpg',
+    thumbnail_image: 'https://cdn.quasar.dev/img/mountains.jpg',
+    views: 56,
   },
   {
     id: 4,
-    content: 'ct4'
+    title: 'Tech Talk: Latest gadgets and innovations',
+    channel: 'TechByte',
+    channel_image: 'https://cdn.quasar.dev/img/avatar2.jpg',
+    thumbnail_image: 'https://cdn.quasar.dev/img/quasar.jpg',
+    views: 78,
   },
   {
     id: 5,
-    content: 'ct5'
-  }
+    title: 'Fitness Tips: Stay healthy and fit',
+    channel: 'FitFam',
+    channel_image: 'https://cdn.quasar.dev/img/avatar2.jpg',
+    thumbnail_image: 'https://cdn.quasar.dev/img/parallax2.jpg',
+    views: 90,
+  },
+  {
+    id: 6,
+    title: "Gaming Fun: Let's play some games!",
+    channel: "Gamer's Paradise",
+    channel_image: 'https://cdn.quasar.dev/img/avatar2.jpg',
+    thumbnail_image: 'https://cdn.quasar.dev/img/parallax1.jpg',
+    views: 102,
+  },
+  {
+    id: 7,
+    title: "Fashion Trends: What's hot right now?",
+    channel: 'Fashionista',
+    channel_image: 'https://cdn.quasar.dev/img/avatar2.jpg',
+    thumbnail_image: 'https://cdn.quasar.dev/img/quasar.jpg',
+    views: 124,
+  },
+  {
+    id: 8,
+    title: 'DIY Crafts: Get creative with crafts',
+    channel: 'Crafty Corner',
+    channel_image: 'https://cdn.quasar.dev/img/avatar2.jpg',
+    thumbnail_image: 'https://cdn.quasar.dev/img/mountains.jpg',
+    views: 146,
+  },
 ]);
-
-const meta = ref<Meta>({
-  totalCount: 1200
-});
 </script>
